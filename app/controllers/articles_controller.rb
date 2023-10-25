33bc622038714d1cd233b048118b1a5d453f359f
dhrumit1 <dhamuparmar105@gmail.com>
2023-10-26 00:33:18 +0530

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: params[:article][:title], body: params[:article][:body])
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(title: params[:article][:title], body: params[:article][:body])
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+end
